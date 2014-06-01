bf06bd8b284035abff9f994b19d4d34bff26fda8
Thuc Le <ledongthuc@gmail.com>
2014-06-01 07:41:23 +0700

    Update: create design with AngularJs, remove old design

---
+++ design2/js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('worldcupResults', [
+  'ngRoute',
+  'worldcupResults.filters',
+  'worldcupResults.services',
+  'worldcupResults.directives',
+  'worldcupResults.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController'});
+  $routeProvider.when('/matches', {templateUrl: 'partials/matches.html', controller: 'MatchesController'});
+  $routeProvider.when('/groups', {templateUrl: 'partials/groups.html', controller: 'GroupController'});
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
+
+// Config menu collapse
+$(document).on('click',function(){
+	$('.collapse').collapse('hide');
+})
---
+++ design2/js/controllers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('worldcupResults.controllers', [])
+  .controller('HomeController', ['$scope', function($scope) {
+
+  }])
+  .controller('MatchesController', ['$scope', function($scope) {
+
+  }])
+  
+  .controller('GroupsController', ['$scope', function($scope) {
+
+  }]);
