c13747ce438abfbf8be2c6b420364dac77d97d00
Thuc Le <ledongthuc@gmail.com>
2014-06-03 00:50:35 +0700

    Update:

--- database/2014_06_01.sql
+++ database/2014_06_01.sql
@@ -29,8 +29,18 @@ CREATE TABLE `group` (
   `name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `order` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `group`
+--
+
+LOCK TABLES `group` WRITE;
+/*!40000 ALTER TABLE `group` DISABLE KEYS */;
+INSERT INTO `group` VALUES (1,'Group A',NULL),(2,'Group B',NULL),(3,'Group C',NULL),(4,'Group D',NULL),(5,'Group E',NULL),(6,'Group F',NULL),(7,'Group G',NULL),(8,'Group H',NULL);
+/*!40000 ALTER TABLE `group` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `match`
@@ -43,26 +53,23 @@ CREATE TABLE `match` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `dateTime` datetime DEFAULT NULL,
+  `team1Id` int(11) DEFAULT NULL,
+  `team2Id` int(11) DEFAULT NULL,
+  `team1Score` int(11) DEFAULT NULL,
+  `team2Score` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `result`
+-- Dumping data for table `match`
 --
 
-DROP TABLE IF EXISTS `result`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `result` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `matchId` int(11) DEFAULT NULL,
-  `teamId` int(11) DEFAULT NULL,
-  `scope` int(11) DEFAULT NULL,
-  `order` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `match` WRITE;
+/*!40000 ALTER TABLE `match` DISABLE KEYS */;
+INSERT INTO `match` VALUES (1,'Group A','2014-06-13 03:00:00',1,2,NULL,NULL),(2,'Group A','2014-06-13 23:00:00',3,4,NULL,NULL),(3,'Group B','2014-06-14 02:00:00',5,6,NULL,NULL),(4,'Group B','2014-06-14 04:00:00',7,8,NULL,NULL),(5,'Group C','2014-06-14 23:00:00',9,10,NULL,NULL),(6,'Group C','2014-06-15 02:00:00',11,12,NULL,NULL),(7,'Group D','2014-06-15 04:00:00',13,14,NULL,NULL),(8,'Group D','2014-06-15 08:00:00',15,16,NULL,NULL),(9,'Group E','2014-06-15 23:00:00',17,18,NULL,NULL),(10,'Group E','2014-06-16 02:00:00',19,20,NULL,NULL),(11,'Group F','2014-06-16 05:00:00',21,22,NULL,NULL),(12,'Group F','2014-06-16 23:00:00',23,24,NULL,NULL),(13,'Group G','2014-06-17 02:00:00',25,26,NULL,NULL),(14,'Group G','2014-06-17 05:00:00',27,28,NULL,NULL),(15,'Group H','2014-06-17 23:00:00',29,30,NULL,NULL),(16,'Group H','2014-06-18 04:00:00',31,32,NULL,NULL),(17,'Group A','2014-06-18 02:00:00',1,3,NULL,NULL),(18,'Group A','2014-06-18 23:00:00',4,2,NULL,NULL),(19,'Group B','2014-06-19 02:00:00',5,7,NULL,NULL),(20,'Group B','2014-06-19 05:00:00',8,6,NULL,NULL),(21,'Group C','2014-06-19 23:00:00',9,11,NULL,NULL),(22,'Group C','2014-06-20 02:00:00',12,8,NULL,NULL),(23,'Group D','2014-06-20 05:00:00',13,15,NULL,NULL),(24,'Group D','2014-06-20 23:00:00',16,14,NULL,NULL),(25,'Group E','2014-06-21 02:00:00',17,19,NULL,NULL),(26,'Group E','2014-06-21 05:00:00',20,18,NULL,NULL),(27,'Group F','2014-06-21 23:00:00',21,23,NULL,NULL),(28,'Group F','2014-06-22 02:00:00',24,22,NULL,NULL),(29,'Group G','2014-06-22 05:00:00',25,27,NULL,NULL),(30,'Group G','2014-06-22 23:00:00',28,26,NULL,NULL),(31,'Group H','2014-06-23 02:00:00',29,31,NULL,NULL),(32,'Group H','2014-06-23 05:00:00',32,30,NULL,NULL),(33,'Group A','2014-06-23 23:00:00',4,1,NULL,NULL),(34,'Group A','2014-06-23 23:00:00',2,3,NULL,NULL),(35,'Group B','2014-06-24 03:00:00',8,5,NULL,NULL),(36,'Group B','2014-06-24 03:00:00',6,7,NULL,NULL),(37,'Group C','2014-06-24 23:00:00',12,9,NULL,NULL),(38,'Group C','2014-06-24 23:00:00',10,11,NULL,NULL),(39,'Group D','2014-06-25 03:00:00',16,13,NULL,NULL),(40,'Group D','2014-06-25 03:00:00',14,15,NULL,NULL),(41,'Group E','2014-06-25 23:00:00',20,17,NULL,NULL),(42,'Group E','2014-06-25 23:00:00',18,19,NULL,NULL),(43,'Group F','2014-06-26 03:00:00',24,21,NULL,NULL),(44,'Group F','2014-06-26 03:00:00',22,23,NULL,NULL),(45,'Group G','2014-06-26 23:00:00',28,25,NULL,NULL),(46,'Group G','2014-06-26 23:00:00',26,27,NULL,NULL),(47,'Group H','2014-06-27 03:00:00',32,29,NULL,NULL),(48,'Group H','2014-06-27 03:00:00',30,31,NULL,NULL),(49,'Row of 16','2014-06-28 23:00:00',NULL,NULL,NULL,NULL),(50,'Row of 16','2014-06-29 03:00:00',NULL,NULL,NULL,NULL),(51,'Row of 16','2014-06-29 23:00:00',NULL,NULL,NULL,NULL),(52,'Row of 16','2014-06-30 03:00:00',NULL,NULL,NULL,NULL),(53,'Row of 16','2014-06-30 23:00:00',NULL,NULL,NULL,NULL),(54,'Row of 16','2014-07-01 03:00:00',NULL,NULL,NULL,NULL),(55,'Row of 16','2014-07-01 23:00:00',NULL,NULL,NULL,NULL),(56,'Row of 16','2014-07-02 03:00:00',NULL,NULL,NULL,NULL),(57,'Quarter-finals','2014-07-04 23:00:00',NULL,NULL,NULL,NULL),(58,'Quarter-finals','2014-07-05 03:00:00',NULL,NULL,NULL,NULL),(59,'Quarter-finals','2014-07-05 23:00:00',NULL,NULL,NULL,NULL),(60,'Quarter-finals','2014-07-06 03:00:00',NULL,NULL,NULL,NULL),(61,'Semi-finals','2014-07-09 03:00:00',NULL,NULL,NULL,NULL),(62,'Semi-finals','2014-07-10 03:00:00',NULL,NULL,NULL,NULL),(63,'Third place','2014-07-13 03:00:00',NULL,NULL,NULL,NULL),(64,'Final','2014-07-14 02:00:00',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `match` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `team`
@@ -76,14 +83,20 @@ CREATE TABLE `team` (
   `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `flag` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `order` int(11) DEFAULT NULL,
-  `teamId` int(11) DEFAULT NULL,
+  `groupId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping events for database 'worldcup-results'
+-- Dumping data for table `team`
 --
+
+LOCK TABLES `team` WRITE;
+/*!40000 ALTER TABLE `team` DISABLE KEYS */;
+INSERT INTO `team` VALUES (1,'Brazil','brazil.png',NULL,1),(2,'Croatia','croatia.png',NULL,1),(3,'Mexico','mexico.png',NULL,1),(4,'Cameroon','cameroon.png',NULL,1),(5,'Spain','spain.png',NULL,2),(6,'Netherlands','netherlands.png',NULL,2),(7,'Chile','chile.png',NULL,2),(8,'Australia','australia.png',NULL,2),(9,'Colombia','colombia.png',NULL,3),(10,'Greece','greece.png',NULL,3),(11,'Ivory Coast','ivory-coast.png',NULL,3),(12,'Japan','japan.png',NULL,3),(13,'Uruguay','uruguay.png',NULL,4),(14,'Costa Rica','costa-rica.png',NULL,4),(15,'England','england.png',NULL,4),(16,'Italy','italy.png',NULL,4),(17,'Switzerland','switzerland.png',NULL,5),(18,'Ecuador','ecuador.png',NULL,5),(19,'France','france.png',NULL,5),(20,'Honduras','honduras.png',NULL,5),(21,'Argentina','argentina.png',NULL,6),(22,'Bosnia and Herzegovina','bosnia-and-herzegovina.png',NULL,6),(23,'Iran','iran.png',NULL,6),(24,'Nigeria','nigeria.png',NULL,6),(25,'Germany','germany.png',NULL,7),(26,'Portugal','portugal.png',NULL,7),(27,'Ghana','ghana.png',NULL,7),(28,'United States','united-states.png',NULL,7),(29,'Belgium','belgium.png',NULL,8),(30,'Algeria','algeria.png',NULL,8),(31,'Russia','russia.png',NULL,8),(32,'South Korea','south-korea.png',NULL,8);
+/*!40000 ALTER TABLE `team` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping routines for database 'worldcup-results'
@@ -98,4 +111,4 @@ CREATE TABLE `team` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-01  8:04:57
+-- Dump completed on 2014-06-02 23:46:42
--- design2/js/controllers.js
+++ design2/js/controllers.js
@@ -3,8 +3,10 @@
 /* Controllers */
 
 angular.module('worldcupResults.controllers', [])
-  .controller('HomeController', ['$scope', function($scope) {
-
+  .controller('HomeController', ['$scope', function($scope, $http) {
+	  $http.get('http://worldcup.thaosin.com/match.php').success(function(data) {
+		  $scope.match = data;
+	  });
   }])
   .controller('MatchesController', ['$scope', function($scope) {
 
