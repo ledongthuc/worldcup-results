20f7c2483cdb81bcd2f48c3915f5511ebdd4bb3e
Thuc Le Dong <ledongthuc@gmail.com>
2014-06-03 18:09:11 +0700

    Update: Correct date in match api, check local timezone

--- server-api/match.php
+++ server-api/match.php
@@ -1,8 +1,13 @@
 <?php
+		$timezoneOffset = -420;
+		if(isset($_GET["tz"])) {
+			$timezoneOffset = $timezoneOffset + (int)$_GET["tz"];
+		}
+		
 	    header('Content-Type: application/json');
 	    $mysqli = new mysqli('localhost','thaosin_worldcup','123qweASDzxc','thaosin_worldcup');
 	    $myArray = array();
-	    if ($result = $mysqli->query("SELECT * FROM `match`")) {
+	    if ($result = $mysqli->query("SELECT id, name, DATE_ADD(`dateTime`, INTERVAL $timezoneOffset MINUTE) as `dateTime`, DATE_ADD(`dateTime`, INTERVAL -600 MINUTE) as `dateTimeLocal` , team1Id, team2Id, team1Score, team2Score FROM `match` order by dateTime")) {
 	        $tempArray = array();
 	        while($row = $result->fetch_object()) {
 	                $tempArray = $row;
