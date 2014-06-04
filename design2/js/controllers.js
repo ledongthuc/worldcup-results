4fd6874da789910e79b9256a91d45f16163e1389
Thuc Le Dong <ledongthuc@gmail.com>
2014-06-04 18:39:54 +0700

    Update: Beer and something like that

--- design2/js/controllers.js
+++ design2/js/controllers.js
@@ -26,7 +26,6 @@ angular.module('worldcupResults.controllers', [])
 		var currentDate = new Date()
 		var currentTimeZone = - currentDate.getTimezoneOffset();
 		var currentDateNow = currentDate.getFullYear() + "-" + currentDate.getMonth() + "-" + currentDate.getDate();
-
 		$http.get('http://worldcup.thaosin.com/match.php?tz=' + currentTimeZone + "&n=" + currentDateNow).success(function(data) {
 			$scope.groups = data;
 		}).
@@ -36,19 +35,21 @@ angular.module('worldcupResults.controllers', [])
 			
   }])
   .controller('GroupsController', ['$scope', '$http', '$resource', function($scope, $http, $resource) {
-
+		
 		$scope.groups = $resource("group.json",{},{
 			query:{method:'GET'}
 		}).get();
   
 		var currentDate = new Date()
 		var currentTimeZone = - currentDate.getTimezoneOffset();
-
 		$http.get('http://worldcup.thaosin.com/group.php').success(function(data) {
 		  $scope.groups = data;
 		}).
 		error(function(data, status, headers, config) {
-		  $scope.showError = true;
+			$scope.showError = true;
 		});
+
+  }])
+  .controller('DonationController', ['$scope', function($scope) {
 
   }]);
