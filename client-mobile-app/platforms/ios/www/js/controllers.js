e3e0811f6749241c2c2f9fd28df6a80dfed89bdc
nhat.thai <yp@yp-tech.pyco.vn>
2014-06-05 12:42:25 +0700

    Build in IOS

---
+++ client-mobile-app/platforms/ios/www/js/controllers.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('worldcupResults.controllers', [])
+  .controller('HomeController', ['$scope', '$http', function($scope, $http) {
+		  
+	  var currentDate = new Date()
+	  var currentTimeZone = - currentDate.getTimezoneOffset();
+	  var currentDateNow = currentDate.getFullYear() + "-" + currentDate.getMonth() + "-" + currentDate.getDate();
+	  
+	  $http.get('http://worldcup.thaosin.com/match.php?tz=' + currentTimeZone + "&h=true&n=" + currentDateNow).success(function(data) 
+		{
+			if(data.lengh > 0) {
+				$scope.groups = data;
+			}
+		}).
+		error(function(data, status, headers, config) {
+			$scope.showError = true;
+		});
+	  
+  }])
+  .controller('MatchesController', ['$scope', '$http', '$resource', function($scope, $http, $resource) {
+		var currentDate = new Date()
+		var currentTimeZone = - currentDate.getTimezoneOffset();
+		var currentDateNow = currentDate.getFullYear() + "-" + currentDate.getMonth() + "-" + currentDate.getDate();
+		$http.get('http://worldcup.thaosin.com/match.php?tz=' + currentTimeZone + "&n=" + currentDateNow).success(function(data) {
+			$scope.groups = data;
+		}).
+		error(function(data, status, headers, config) {
+			$scope.showError = true;
+		});
+  }])
+  .controller('GroupsController', ['$scope', '$http', '$resource', function($scope, $http, $resource) {
+		
+		var currentDate = new Date()
+		var currentTimeZone = - currentDate.getTimezoneOffset();
+		$http.get('http://worldcup.thaosin.com/group.php').success(function(data) {
+		  $scope.groups = data;
+		}).
+		error(function(data, status, headers, config) {
+			$scope.showError = true;
+		});
+  }])
+  .controller('DonationController', ['$scope', function($scope) {
+
+  }]);
